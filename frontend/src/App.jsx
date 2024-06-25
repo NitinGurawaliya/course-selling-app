944a017ae155c547773566e3ac188f501d60142d
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2024-06-26 01:35:57 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
+import Landing from "./pages/Landing"
+import Signup from "./pages/Signup"
 
-
+import {BrowserRouter, Route,Routes} from "react-router-dom" 
 function App() {
 
   return (
     <>
-    <div className='py-9'>
-      Hekki 
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/landing" element={<Landing />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
