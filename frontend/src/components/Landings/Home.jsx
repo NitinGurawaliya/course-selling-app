04d8d5b433c319ac7942b2a1979060c6fb131424
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2024-06-26 01:35:41 +0530

    Create Home.jsx

---
+++ frontend/src/components/Landings/Home.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+import {useNavigate,Link} from "react-router-dom"
+
+const Home = () => {
+  const navigate= useNavigate();
+  
+  const signupHandler =()=>{
+    navigate("/signup")
+  }
+  return (
+    <section id="home" className="bg-slate-300 h-screen flex flex-col justify-center items-center">
+      <h1 className="text-5xl font-bold mb-4">Welcome to Courser</h1>
+      <p className="text-xl mb-8">The best place to learn and grow.</p>
+      <div className='space-x-10'>
+      <button onClick={signupHandler} className="bg-blue-600 font-bold text-white px-6 py-3 rounded-full">Sign up</button>
+      <button className="bg-blue-600 font-bold text-white px-6 py-3 rounded-full">Sign in</button>
+      </div>
+      
+    </section>
+  );
+};
+
+export default Home;
