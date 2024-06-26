8333889ecfdbaa68b0c66762a0ac87fb1956f3cc
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2024-06-26 18:21:53 +0530

    Create Inputbox.jsx

---
+++ frontend/src/components/Signup/Inputbox.jsx
@@ -0,0 +1,9 @@
+export function InputBox({onChange,label,placeholder}){
+    return <div>
+        <div className="flex justify-start mt-3 md-2 ">
+            {label}
+        </div>
+        <input onChange={onChange} placeholder={placeholder} className="px-1 border-2   py-1 w-full border-green-400 rounded-lg" />
+       
+    </div>
+} 
