8976fbe237c88cecb5c0fb6157f685f139bf7eed
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2024-06-26 18:22:05 +0530

    Update Navbar.jsx

--- frontend/src/components/Landings/Navbar.jsx
+++ frontend/src/components/Landings/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav className="bg-blue-500 fixed top-0 right-0 left-0 h-15 p-4">
+    <nav className="bg-black fixed top-0 right-0 left-0 h-15 p-4">
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-white font-bold text-xl">Courser</div>
         <div className="space-x-10">
