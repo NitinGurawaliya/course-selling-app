a9d6eb89e6fccbe5e0341cce432786497162b30e
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2024-06-26 18:22:08 +0530

    Update Home.jsx

--- frontend/src/components/Landings/Home.jsx
+++ frontend/src/components/Landings/Home.jsx
@@ -7,13 +7,17 @@ const Home = () => {
   const signupHandler =()=>{
     navigate("/signup")
   }
+
+  const signinHandler=()=>{
+    navigate("/signin")
+  }
   return (
     <section id="home" className="bg-slate-300 h-screen flex flex-col justify-center items-center">
       <h1 className="text-5xl font-bold mb-4">Welcome to Courser</h1>
       <p className="text-xl mb-8">The best place to learn and grow.</p>
       <div className='space-x-10'>
-      <button onClick={signupHandler} className="bg-blue-600 font-bold text-white px-6 py-3 rounded-full">Sign up</button>
-      <button className="bg-blue-600 font-bold text-white px-6 py-3 rounded-full">Sign in</button>
+      <button onClick={signupHandler} className="bg-green-500 font-bold text-white px-6 py-3 rounded-full">Sign up</button>
+      <button onClick={signinHandler} className="bg-green-500 font-bold text-white px-6 py-3 rounded-full">Sign in</button>
       </div>
       
     </section>
