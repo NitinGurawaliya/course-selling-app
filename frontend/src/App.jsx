58a4a52e69f49c77428a468b4fbdec78ed049a73
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2024-06-26 18:21:37 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import Landing from "./pages/Landing"
 import Signup from "./pages/Signup"
+import Signin from "./pages/Signin"
+import Dashboard from "./pages/Dashboard"
 
 import {BrowserRouter, Route,Routes} from "react-router-dom" 
 function App() {
@@ -10,6 +12,10 @@ function App() {
       <Routes>
         <Route path="/landing" element={<Landing />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+
+
       </Routes>
     </BrowserRouter>
     </>
