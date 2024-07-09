63fc1fc7a55ab65a50e83042da910859ede96472
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2024-07-10 01:08:38 +0530

    Update Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { Sidebar } from '../components/Dashboard/Sidebar';
+import Dashnav from '../components/Dashboard/Dashnav';
+import Coursecard from '../components/Dashboard/Coursecard';
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
+    <div>
+      <Sidebar />
+      <Dashnav />
+      <div className="ml-64 mt-20 p-4">
+        <div className="grid grid-cols-3 gap-2">
+          <Coursecard />
+          <Coursecard />
+          <Coursecard />
+          <Coursecard />
+          <Coursecard />
+          <Coursecard />
+
+          <Coursecard />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
