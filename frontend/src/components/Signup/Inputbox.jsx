9f5c24498d97b2dad8c231b95adafecc19507ed5
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2024-07-10 01:08:57 +0530

    Update Inputbox.jsx

--- frontend/src/components/Signup/Inputbox.jsx
+++ frontend/src/components/Signup/Inputbox.jsx
@@ -3,7 +3,14 @@ export function InputBox({onChange,label,placeholder}){
         <div className="flex justify-start mt-3 md-2 ">
             {label}
         </div>
+   
+        <div>
+        
+
         <input onChange={onChange} placeholder={placeholder} className="px-1 border-2   py-1 w-full border-green-400 rounded-lg" />
+
+        </div>
+        
        
     </div>
 } 
