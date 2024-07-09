a397ccd5e54ab50df50920d6f3d2cd3475713b9b
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2024-07-10 01:08:44 +0530

    Update Signin.jsx

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Heading } from '../components/Signup/Heading'
 import { Subheading } from '../components/Signup/Subheading'
 import { InputBox } from '../components/Signup/Inputbox'
 import { Button } from '../components/Signup/Button'
 import { BottomWarning } from '../components/Signup/BottomWarning'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const Signup = () => {
+
+  const navigate  = useNavigate()
+  const [email,setEmail]= useState("");
+  const [password,setPassword] = useState("")
+
+  async function userHandler(){
+    const res= await axios.post("http://localhost:3000/user/signin",{
+      email,
+      password
+    })
+
+    if(res.data.token&&res.data){
+      localStorage.setItem("token",res.data.token)
+      navigate("/dashboard")
+    }
+    else{
+      alert(res.data.msg)
+    }
+
+
+  }
+
+
   return <div className='flex'>
   <div className='bg-green-400  w-1/2 h-screen'>
   Testimonial section
@@ -16,9 +41,14 @@ const Signup = () => {
   <div className="rounded-lg border-green-400 border-2 bg-white w-80 text-center p-2 h-max px-4">
     <Heading label={"Sign in"} />
     <Subheading label={"Enter your information to sign in"} />
-    <InputBox onChange={onchange} label={"Username"} placeholder={"abcd@gmail.com"} />
-    <InputBox onChange={onchange} label={"Password"} placeholder={"********"} />
-    <Button  label={"Sign in as Student"} />
+    <InputBox onChange={(e)=>{
+      setEmail(e.target.value)
+    }} label={"Email"} placeholder={"abcd@gmail.com"} />
+
+    <InputBox onChange={(e)=>{
+      setPassword(e.target.value)
+    }} label={"Password"} placeholder={"********"} />
+    <Button onClick={userHandler}  label={"Sign in as Student"} />
     <Button  label={"Sign in as Instructor"} />
     <BottomWarning label={"Dont't have an account?"} to={"/signup"} buttonText={"signin"}/>
   </div>
