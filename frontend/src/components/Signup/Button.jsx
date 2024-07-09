a587c5d4f058906ff5c5d20fee4defa87a1383c7
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2024-07-10 01:08:52 +0530

    Update Button.jsx

--- frontend/src/components/Signup/Button.jsx
+++ frontend/src/components/Signup/Button.jsx
@@ -1,8 +1,11 @@
-export function Button({onClick,label}){
-    return <button onClick={onClick} type="button" className=" mt-3 bg-green-400 text-white w-full h-8  rounded-lg text-sm hover:bg-green-600 active:bg-slate-50
-    active:text-black">
+export function Button({ onClick, label }) {
+    return (
+        <button 
+            onClick={onClick} 
+            type="button" 
+            className="mt-3  text-black w-full h-8 rounded-lg  text-sm hover:bg-green-400 active:bg-slate-50 active:text-black"
+        >
             {label}
         </button>
- 
-
+    );
 }
